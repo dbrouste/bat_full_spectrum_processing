c4c7a977a1078853f9ce61074b4907f9035709d3
dbrouste <49265712+dbrouste@users.noreply.github.com>
2026-08-21 13:01:06 +1100

    Expose folder chooser helpers

--- annotation/__init__.py
+++ annotation/__init__.py
@@ -1,3 +1,10 @@
 from .app import AppConfig, build_app, run_annotator
+from .launcher import choose_wav_folder, run_annotator_dialog
 
-__all__ = ["AppConfig", "build_app", "run_annotator"]
+__all__ = [
+    "AppConfig",
+    "build_app",
+    "run_annotator",
+    "choose_wav_folder",
+    "run_annotator_dialog",
+]
