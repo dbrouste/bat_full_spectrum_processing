910c1e52af5cba8840ecf1bd47ef3b791ba2f385
dbrouste <49265712+dbrouste@users.noreply.github.com>
2026-08-23 14:24:45 +1100

    Expose benchmark runner and matching API

--- benchmark/__init__.py
+++ benchmark/__init__.py
@@ -1,5 +1,7 @@
 from .curve_metrics import CurveMetrics, compare_curves
 from .detection_metrics import DetectionMetrics, interval_iou, match_intervals
+from .matching import Match, match_chirps
+from .runner import BenchmarkResult, load_analysis_module, run_benchmark
 
 __all__ = [
     "CurveMetrics",
@@ -7,4 +9,9 @@ __all__ = [
     "DetectionMetrics",
     "interval_iou",
     "match_intervals",
+    "Match",
+    "match_chirps",
+    "BenchmarkResult",
+    "load_analysis_module",
+    "run_benchmark",
 ]
